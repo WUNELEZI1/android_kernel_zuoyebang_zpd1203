3b0d2bc5e4d0eca05dc6069e8e0e4e29a73a4123
haiyong.tang <haiyong.tang@mediatek.com>
2025-02-05 15:53:12 +0800

    [ALPS09724791] Fix KE issue
    [Description]
    Add dual pipe judgment
    
    [Test]
    Build pass
    
    MTK-Commit-Id: 0476c6876311333744e74418d5942355dc84072c
    
    Signed-off-by: haiyong.tang <haiyong.tang@mediatek.com>
    CR-Id: ALPS09724791
    Feature: display_pq
    Change-Id: Ia27202f3aad5c2ddec90228ae4e30b2971550a62
    (cherry picked from commit bcd6f171c81ffc58bcf5a23e64a548bd436ca971)

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_ccorr.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_ccorr.c
@@ -1870,10 +1870,13 @@ int disp_ccorr_set_bypass(struct drm_crtc *crtc, int bypass)
 {
 	int ret = 0;
 
-	if (g_ccorr_relay_value[index_of_ccorr(default_comp->id)] == bypass &&
-		g_ccorr_relay_value[index_of_ccorr(ccorr1_default_comp->id)] == bypass)
+	if (g_ccorr_relay_value[index_of_ccorr(default_comp->id)] == bypass)
 		return ret;
+
 	ret = mtk_crtc_user_cmd(crtc, default_comp, BYPASS_CCORR, &bypass);
+	if ((disp_ccorr_number == 2 || default_comp->mtk_crtc->is_dual_pipe) && ccorr1_default_comp)
+		ret = mtk_crtc_user_cmd(crtc, ccorr1_default_comp, BYPASS_CCORR, &bypass);
+
 	DDPINFO("%s : ret = %d", __func__, ret);
 	return ret;
 }
